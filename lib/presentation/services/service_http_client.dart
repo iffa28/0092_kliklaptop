4d6f989860df36f8e6727fc07436c175dde2200e
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-06-29 12:56:48 +0700

    Menambahkan class untuk mengirim data ke server dan mengambil data, dengan atau tanpa token.

---
+++ lib/presentation/services/service_http_client.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class ServiceHttpClient {
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
+  final secureStorage = FlutterSecureStorage();
+
+  //post
+  Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
+    }
+  }
+
+  //postwithtoken
+  Future<http.Response> postWithToken(
+    String endPoint,
+    Map<String, dynamic> body,
+  ) async {
+    //buat var token yang baca dari secure storage
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
+    }
+  }
+
+  //get
+  Future<http.Response> get(String endPoint) async {
+    //buat var token yang baca dari secure storage
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      );
+      return response;
+    } catch (e) {
+      throw Exception("GET request failed: $e");
+    }
+  }
+}
